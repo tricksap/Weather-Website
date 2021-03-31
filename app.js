338c2fe66c0716f20609534ffb5671412c8a6fdf
Patrick <tricksap@gmail.com>
2021-03-31 09:47:07 +0800

    change icons and add daily weather forecast

--- app.js
+++ app.js
@@ -10,25 +10,29 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get("/", function (req, res) {
-//   res.render("home");
-// });
-
 app.get("/", function (req, res) {
   const apiKey = process.env.API_KEY;
   const unit = "metric";
+
   const url =
     "https://api.openweathermap.org/data/2.5/onecall?lat=14.599512&lon=120.984222&exclude=hourly,minutely,alerts&units=" +
     unit +
     "&appid=" +
     apiKey;
   https.get(url, function (response) {
-    console.log(response.statusCode);
+    day = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
-      console.log(weatherData);
-      const todayWeather = weatherData.current.temp;
-      const timeZone = res.render("home", { weatherData: weatherData });
+      // console.log(weatherData);
+      res.render("home", { weatherData: weatherData });
     });
   });
 });
